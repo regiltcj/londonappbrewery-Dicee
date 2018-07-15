d6e2dbaab347b00fb9be3a1159d79857cd757ceb
C J <C J>
2018-07-15 12:30:06 +0530

    Initial commit

---
+++ app/src/main/java/com/cj/dicee/MainActivity.java
@@ -0,0 +1,44 @@
+package com.cj.dicee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button rollButton = findViewById(R.id.buttonRoll);
+
+        final ImageView leftDice = findViewById(R.id.imageleftDice);
+        final ImageView rightDice  = findViewById(R.id.imageRightDice);
+
+        final int[] diceArray = {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6,
+        };
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random randomNumberGenerator = new Random();
+                int number = randomNumberGenerator.nextInt(6);
+                leftDice.setImageResource(diceArray[number]);
+                number = randomNumberGenerator.nextInt(6);
+                rightDice.setImageResource(diceArray[number]);
+            }
+        });
+    }
+}
